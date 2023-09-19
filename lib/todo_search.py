fe9e559e4be474892ab3896ef4b860e43f24f2a3
Sandra Soinska <sandra.soinska@gmail.com>
2023-09-19 16:23:54 +0100

    Completed challenge containing tests and code

--- lib/todo_search.py
+++ lib/todo_search.py
@@ -1,3 +1,8 @@
+#function that returns 'Yes' if #TODO is in the text
 def includes_todo(text):
     if text == "":
         raise Exception("No text provided")
+    elif "#TODO" in text:
+        return "Yes"
+    else:
+        return "No"
--- tests/test_includes_todo.py
+++ tests/test_includes_todo.py
@@ -9,3 +9,21 @@ def test_empty_string():
         error_message = str(e.value)
         assert error_message == "No text provided"
 
+#Text containing #TODO returns "Yes"
+string = 'Example text #TODO'
+def test_todo_in_text():
+    result = includes_todo(string)
+    assert result == "Yes"
+
+#Text containing #Todo in lowecase returns "No"
+def test_todo_lower_in_text():
+    result = includes_todo("Example text #Todo")
+    assert result == "No"
+
+#Text not containing #TODO returns "No"
+def test_todo_not_in_text():
+    result = includes_todo("Example text.")
+    assert result == "No"
+
+
+
